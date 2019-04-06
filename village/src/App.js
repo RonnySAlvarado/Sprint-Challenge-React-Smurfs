4f718e7e937f792602b9c291d068351f9ff5271b
Ronny Alvarado <rsalvarado777@gmail.com>
2019-04-06 12:33:19 -0500

    Created a deleteSmurf method to delete any of the Smurfs on your list

--- village/src/App.js
+++ village/src/App.js
@@ -34,6 +34,15 @@ class App extends Component {
       .catch(err => console.log(err));
   };
 
+  deleteSmurf = id => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => console.log(err));
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -54,7 +63,13 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              deleteSmurf={this.deleteSmurf}
+            />
+          )}
         />
       </div>
     );
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -7,6 +7,9 @@ const Smurf = props => {
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button onClick={() => props.deleteSmurf(props.id)} className="delete">
+        Delete
+      </button>
     </div>
   );
 };
